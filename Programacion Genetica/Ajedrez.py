f6f29db4218ae68800dae5e85e3f0dfa337c2d94
Erik Manuel <2526_eriksaldana@iticbcn.cat>
2025-10-06 20:41:33 +0200

    Refactor Ajedrez.py: Improve population initialization, enhance crossover function, and refine fitness calculation

--- Programacion Genetica/Ajedrez.py
+++ Programacion Genetica/Ajedrez.py
@@ -2,9 +2,15 @@ import random
 
 # Utilitzant programació genetica, crea un algorisme que trobi una possible solució del problema de les 8 reinas generalitzada per taules d’escacs de NxN.
 
-mutacion = 10
-gen_actual = 75
-gen_ant = 25
+# Mutacion = 10%
+
+# Reparticion de poblacion entre cada generacion:
+# Generacion actual = 75%
+# Generacion anterior = 25%
+
+# Mejores fit = 65%
+# Peores fit = 25%
+# Mutaciones = 10%
 
 # Funcion para generar individuos - LISTO
 def init_individu(mida_tauler):
@@ -13,15 +19,15 @@ def init_individu(mida_tauler):
 # Funcion para iniciar la poblacion con la medida del tablero - LISTO
 def init_population(mida_tauler, mida_pop):
     poblacion_actual = []
-    poblacion_anterior = []
 
-    for individu in range(mida_pop):
+    for i in range(mida_pop):
         poblacion_actual.append(init_individu(mida_tauler))
 
     print(poblacion_actual)
-    fitness(mida_tauler, poblacion_actual)
 
-# Funcion para mezclar padres y generar hijos - FALTA MUTACIÓN
+    return poblacion_actual
+
+# Funcion para mezclar padres y generar hijos - LISTO
 def crossover(individu1, individu2):
     new_ind = []
     mitad = len(individu1) // 2
@@ -33,28 +39,66 @@ def crossover(individu1, individu2):
         if i not in new_ind:
             new_ind.append(i)
 
+    if random.randint(1, 100) <= 10:
+        i, j = random.sample(range(len(new_ind)), 2)
+        new_ind[i], new_ind[j] = new_ind[j], new_ind[i]    
+        print(f"Individuo mutado: {new_ind}")
+
     return new_ind
 
 # Funcion para calcular el fitness de cada individuo - LISTO
-def fitness(mida_tauler, pop):
+def fitness(mida_tauler, poblacion_actual):
     fitness_actual = []
-    fitness_anterior = []
 
-    for individu in pop:
+    for individu in poblacion_actual:
         choques = 0
         for i in range(mida_tauler):
             for j in range(i + 1, mida_tauler):
                 if abs(individu[i] - individu[j]) == abs(i - j):
                     choques += 1
         fitness_actual.append(choques)
-
-    print(fitness_actual)
-    return
+        
+        if choques == 0:
+            print(f"Este es uno de los posibles resultados: {individu}")
+            return fitness_actual, True
+    
+    return fitness_actual, False
 
 # Funcion para elegir los individuos que permanecerán en la siguiente generacion
-def selection(fit_actual):
-    for i in fit_actual:
-        print(i)
-    return
+def selection(pop_actual, fit_actual, pop_anterior = None, fit_anterior = None):
+    new_poblation = []
+    #for i in fit_actual:
+    
+    #for i in fit_anterior:
 
-init_population(8, 100)
+    return new_poblation
+
+# Funcion main que ejecuta todas las funciones
+def main_algorithm(mida_tauler, mida_pop):
+    poblacion_actual = init_population(mida_tauler, mida_pop)
+    poblacion_anterior = []
+
+    fitness_actual, resuelto = fitness(mida_tauler, poblacion_actual)
+    if resuelto: return
+
+    fitness_anterior = []
+
+    selection(poblacion_actual, fitness_actual)
+
+    while resuelto == False:
+
+        nueva_generacion = []
+        for _ in range(mida_pop):
+            padre1 = random.choice(poblacion_actual)
+            padre2 = random.choice(poblacion_actual)
+            nueva_generacion.append(crossover(padre1, padre2))
+
+        poblacion_anterior = poblacion_actual
+        poblacion_actual = nueva_generacion
+
+        fitness_anterior = fitness_actual
+        fitness_actual, resuelto = fitness(mida_tauler, nueva_generacion)
+
+        selection(poblacion_actual, fitness_actual, poblacion_anterior, fitness_anterior)
+
+main_algorithm(8, 100)
