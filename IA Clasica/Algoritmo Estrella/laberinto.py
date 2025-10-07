9960d6a988c5951e1eb6a792bf5ef28a49d893dc
Erik Manuel <2526_eriksaldana@iticbcn.cat>
2025-10-07 20:58:47 +0200

    add laberinto.py and the library

---
+++ IA Clasica/Algoritmo Estrella/laberinto.py
@@ -0,0 +1,25 @@
+import pyamaze as maze
+from queue import PriorityQueue
+import time
+
+ROWS = 20
+COLS = 20
+
+def distance(cell1, cell2):
+    return 
+
+def aStar(m):
+    forwardPath = ""
+    return forwardPath
+
+m=maze.maze(ROWS,COLS)
+m.CreateMaze()
+# pre_Astar = time.time()
+# path = aStar(m)
+# post_Astar = time.time()
+# print(post_Astar - pre_Astar)
+# a=maze.agent(m,footprints=True)
+# m.tracePath({a:path},delay=5)
+m.run()
+
+# Crea un algoritme que busqui la sortida d'un laberint utilitzant A*
